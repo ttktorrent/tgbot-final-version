c961c71eb1a8a500892e107488bc985b1e54d4bb
ttktorrent <78251394+ttktorrent@users.noreply.github.com>
2021-02-02 12:38:48 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -5,10 +5,10 @@ except:
         # TODO optimize for vps use fully - currently only heroku is focused
         # Set true if its VPS [currently not fully working]
         IS_VPS = False
-        API_HASH = "d0def2fadec58c31403bf01a71454019"
-        API_ID = 2876934
-        BOT_TOKEN = "1516701846:AAFkv49CM7RToxxInvAWJDGK90DPO5RAAOo"
-        BASE_URL_OF_BOT = "https://nobitest005.herokuapp.com"
+        API_HASH = "d0def2fadec5c31403bf01a71454019"
+        API_ID = 287934
+        BOT_TOKEN = "1516701846:AAFkv49M7RToxxInvAWJDGK90DPO5RAAOo"
+        BASE_URL_OF_BOT = "https://nobitst005.herokuapp.com"
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
         ALD_USR = [1458912827,1467018377,1546675358,1444150889,1482853408,1333689035,935041758,885488992,-100374414317,-1001479070385,-100320640070,-1001422326809,-1001267728460]
         
@@ -31,7 +31,7 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DB_URI = "postgres://iuzrmttujpmprj:a5e4544226f9c2c215cc7e088343b8828920e20c9d70f3e1d5b043b26f748265@ec2-50-16-108-254.compute-1.amazonaws.com:5432/ddjad561tteob2"
+        DB_URI = "postgres://iuujpmprj:a5e4544226f9c2c215cc7e088343b8828920e20c9d70f3e1d5b043b26f748265@ec2-50-16-108-254.compute-1.amazonaws.com:5432/ddjad561tteob2"
         
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
         #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
